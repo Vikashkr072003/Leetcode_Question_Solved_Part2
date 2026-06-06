59132fb9657eebe0daeeefde2cabfac917dee324
Vikash kumar <vikashkr072003@gmail.com>
2026-06-06 16:59:27 +0000

    validPalindrome

---
+++ question_02.py
@@ -0,0 +1,29 @@
+# Question : - 125. Valid Palindrome
+
+# Problem :- A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+# ----------------------------------------------------------------------------------------
+
+# Code :-------
+
+
+# def validPalindrome(str):
+#     l = 0
+#     r = len(str) - 1
+
+#     while l < r:
+#         if str[l] == str[r]:
+#             l += 1
+#             r -= 1
+#         else:
+#             return False
+
+#     return True
+
+
+# print(validPalindrome("121"))  True
+# print(validPalindrome("hello")) False
+
+
