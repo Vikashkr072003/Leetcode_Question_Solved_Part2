0850a44d930030f89a7c5e3f9bdf192aa069edc1
Vikash kumar <vikashkr072003@gmail.com>
2026-07-02 13:05:21 +0000

    Number of Good Pairs.

---
+++ HashMap_Questions/Question_03.py
@@ -0,0 +1,50 @@
+# Questions :- 1512. Number of Good Pairs
+# Problems :- Given an array of integers nums, return the number of good pairs.
+# A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+# Example 1:
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+
+# Example 2:
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+
+
+# Example 3:
+# Input: nums = [1,2,3]
+# Output: 0
+
+# Code :-----
+
+
+def numberOfGood(nums):
+    count = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+
+    return count
+
+
+nums = [1, 2, 3, 1, 1, 3]
+print(numberOfGood(nums))
+
+
+def numberOfGood(nums):
+    freq = {}
+
+    for num in nums:
+        freq[num] = freq.get(num, 0) + 1
+
+    return sum(k * (k - 1) // 2 for k in freq.values())
+
+
+nums = [1, 2, 3, 1, 1, 3]
+
+print(numberOfGood(nums))
