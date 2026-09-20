78b946fc2972359ea1cd48a93fd0a4b161467e65
Vikash kumar <vikashkr072003@gmail.com>
2026-09-20 09:46:19 +0000

    739.Daily Temperatures.

---
+++ Stack_Questions/Monotonic_Stack/Question02.py
@@ -0,0 +1,39 @@
+# Questions :--- 739.Daily Temperatures
+"""
+Problem :--
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+
+Example 1:
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+
+Example 2:
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+
+Example 3:
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+
+"""
+
+# Code :--
+
+
+def dailyTemp(temperature):
+    stack = []
+    result = [0] * len(temperature)
+
+    for i in range(len(temperature)):
+        while stack and temperature[i] > temperature[stack[-1]]:
+            prev_index = stack.pop()
+            result[prev_index] = i - prev_index
+
+        stack.append(i)
+
+    return result
+
+
+temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+print(dailyTemp(temperatures))
